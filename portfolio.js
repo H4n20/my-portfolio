5b590c10fb933a31de53f94d61b96232856063f9
dolinh <dolinh7141@gmail.com>
2025-02-05 17:50:45 +0700

    add back to top butotn

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,19 @@
+// back to top button
+window.onscroll = function() {
+    let btn = document.getElementById("backToTop");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+// back to top eff
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+// menu eff
 document.querySelectorAll('a.nav-link').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
